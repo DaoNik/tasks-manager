57febe518dc2100aebced27ce19cc97938217f63
DaoNik <nikitataranin@gmail.com>
2022-05-18 20:06:18 +0300

    добавили шаблон kanban доски

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './header/sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
+import { FormIssueComponent } from './form-issue/form-issue.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SidebarComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainComponent,
+    FormIssueComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/form-issue/form-issue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-issue',
+  templateUrl: './form-issue.component.html',
+  styleUrls: ['./form-issue.component.scss']
+})
+export class FormIssueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
