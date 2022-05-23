891a6c8bd5781a17c34c1e4f8655074e43ce9b4f
Leonid (notebook) <leonidrawhide@gmail.com>
2022-05-23 16:27:24 +0300

    added possibility to add data to modal

--- src/app/modal-task/modal-task.component.ts
+++ src/app/modal-task/modal-task.component.ts
@@ -1,7 +1,8 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { ITaskData } from './modal-task-interface';
 import { FormBuilder } from '@angular/forms';
+import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-modal-task',
@@ -22,17 +23,78 @@ export class ModalTaskComponent implements OnInit {
       }]]
     ,
     description: ['Короткое описание задачи'],
-    text: ['Длинное описание задачи']
+    text: [[{
+      text: 'Пункт 1', 
+      type: 'ul',
+      value: 'false'
+    },{
+      text: 'Пункт 2', 
+      type: 'todo',
+      value: 'true'
+    }]]
   });
+  typeOptions = [{
+    name: 'text',
+    type: '',
+    value: ''
+  },
+  {
+    name: 'ToDo',
+    type: 'todo',
+    value: 'false'
+  },
+  {
+    name: 'List',
+    type: 'ul',
+    value: ''
+  }]
+  inputTrigger = false;
+  createOption = {
+    name: 'text',
+    type: '',
+    value: ''
+  }
 
   constructor(
     public dialogRef: MatDialogRef<ModalTaskComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private fb: FormBuilder
   ) {}
+  
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+  
+  dummy(a: any) {
+    console.log(a)
+  }
+
+  addText(e: any, num: number) {
+    if (num == -1) {
+      this.mockTaskData.value.text.push({
+        text: e.target.value, 
+        type: this.createOption.type,
+        value: this.createOption.value
+      })
+      e.target.value = '';
+      this.createOption.type = '';
+      this.createOption.value = '';
+    } else {
+      this.mockTaskData.value.text[num].text = e.target.value;
+    }
+    console.log(this.mockTaskData.value.text)
+  }
+
+  changeType(option?: any, index?: number) {
+    if (index && option){
+      this.mockTaskData.value.text[index].type = option.type;
+      this.mockTaskData.value.text[index].value = option.value;
+    } else {
+      this.createOption.type = option.type;
+      this.createOption.value = option.value;
+    }
+    
   }
 
   ngOnInit(): void {
